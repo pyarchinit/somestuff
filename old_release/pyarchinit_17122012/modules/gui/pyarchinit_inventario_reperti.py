addd9a9abe6c9a54437ba7efcef636dc9181013f
Luca Mandolesi <pyarchinit@gmail.com>
2015-07-07 14:41:35 +0200

    not import

--- old_release/pyarchinit_17122012/modules/gui/pyarchinit_inventario_reperti.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-
-# Form implementation generated from reading ui file 'pyarchinit_inventario_reperti.ui'
-#
-# Created: Tue Jul 31 22:46:47 2012
-#      by: PyQt4 UI code generator 4.8.3
-#
-# WARNING! All changes made in this file will be lost!
-
--- old_release/pyarchinit_3dmatplotlibtest/modules/db/query_sql/pyarchinit_create_db.sql
+++
@@ -1,7 +0,0 @@
-CREATE DATABASE pyarchinit
-  WITH OWNER = postgres
-       ENCODING = 'UTF8'
-       LC_COLLATE = 'C'
-       LC_CTYPE = 'C'
-       CONNECTION LIMIT = -1
-       TEMPLATE = template_postgis;
